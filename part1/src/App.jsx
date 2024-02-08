47703a24e66bb014c83d1845d8cf72cdb405ab08
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-02-08 16:38:01 -0500

    Add props in Hello component

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,19 +1,22 @@
-const Hello = () => {
+const Hello = (props) => {
+console.log(props)
 return (
 <div>
-<p>Hello world</p>
+<p>Hello {props.name}, you are {props.age} years old</p>
 </div>
 )
 } 
 
 
 const App = () => {
+const name = 'Peter'
+const age = 20
 return (
     <div>
       <h1>Greetings</h1>
-<Hello />
-<Hello />
-<Hello />    
+<Hello name="George" age={10+2}/>
+<Hello name="Daisy" age={age}/>
+<Hello name={name} />    
 </div>
   )
 }
