d93e06ba53aba46a75cc39657e348931fa250396
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-02-08 17:29:45 -0500

    render an array

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,23 +1,10 @@
-const Hello = (props) => {
-console.log(props)
-return (
-<div>
-<p>Hello {props.name}, you are {props.age} years old</p>
-</div>
-)
-} 
-
-
 const App = () => {
-const name = 'Peter'
-const age = 20
-return (
-    <div>
+  const friends = [ 'Peter', 'Maya' ];
+  return (
+    <>
       <h1>Greetings</h1>
-<Hello name="George" age={10+2}/>
-<Hello name="Daisy" age={age}/>
-<Hello name={name} />    
-</div>
+      <p>{friends}</p>
+    </>
   )
 }
 
