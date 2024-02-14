25d9b4eeda684d5099d4ce37fa6538245ec22737
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-02-14 11:22:44 -0500

    Simulate the re-render of the component with a counter

--- part1/part1a/src/App.jsx
+++ part1/part1a/src/App.jsx
@@ -1,28 +1,9 @@
-const Hello = ({name, age}) => {
-  // component helper function
-  const bornYear = () => new Date().getFullYear() - age;
-
+const App = (props) => {
+  const {counter} = props;
+  
   return (
     <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you are probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
-
-
-
-const App = () => {
-  const name = 'Peter';
-  const age = 10;
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26+10} />
-      <Hello name={name} age={age} />
+      {counter}
     </div>
   )
 }
--- part1/part1a/src/main.jsx
+++ part1/part1a/src/main.jsx
@@ -1,6 +1,19 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+let counter = 1;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+ 
+const refresh = () => { 
+    root.render(
+        <App counter={counter} />
+    ) 
+ }
+
+ refresh();
+
+ setInterval(() => { 
+    refresh();
+    counter ++;
+  }, 1000)
