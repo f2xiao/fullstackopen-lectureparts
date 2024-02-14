c898da920b194e22cea8865286cd53db5e312146
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-02-14 11:56:31 -0500

    Add state hook

--- part1/part1a/src/App.jsx
+++ part1/part1a/src/App.jsx
@@ -1,5 +1,16 @@
-const App = (props) => {
-  const {counter} = props;
+import { useState } from "react";
+
+
+
+const App = () => {
+  // adds state to the component and renders it initialized with the value of zero
+  const [counter, setCounter] = useState(0);
+
+  // execute the setTimeout
+  setTimeout(
+    () => setCounter(counter + 1), // increment the counter state
+    1000 // every 1s
+  );
   
   return (
     <div>
--- part1/part1a/src/main.jsx
+++ part1/part1a/src/main.jsx
@@ -1,19 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-let counter = 1;
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+) ;
  
-const refresh = () => { 
-    root.render(
-        <App counter={counter} />
-    ) 
- }
-
- refresh();
-
- setInterval(() => { 
-    refresh();
-    counter ++;
-  }, 1000)
