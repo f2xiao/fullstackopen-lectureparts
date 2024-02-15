2bdc9d3e138d92c6e5137ea7e4ea722906671c36
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-02-15 17:20:57 -0500

    A function that returns a function

--- part1/part1a/src/App.jsx
+++ part1/part1a/src/App.jsx
@@ -1,50 +1,18 @@
 import { useState } from "react";
 
-
-const History = ({allClicks}) => {
-  if(allClicks.length == 0){
-    return (
-      <p>the app is used by pressing the buttons</p>
-    )
-  }
-  return (
-    <p>button press history: {allClicks.join('')}</p>
-  )
-}
-
-
-const Button = ({handleClick, text}) => {
-  return (
-    <button onClick={handleClick} >{text}</button>
-  )
-}
-
 const App = () => {
   // adds state to the component and renders it initialized with the value of zero
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-
+  const [value, setValue] = useState(0)
   // event handlers
-  const handleLeft = () => { 
-    const updatedLeft = left + 1;
-    setLeft(updatedLeft);
-    setAll(allClicks.concat('L')) 
-  }
-  const handleRight = () => { 
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-    setAll(allClicks.concat('R')) 
-  }
+ const setToValue = (newValue) => () =>  setValue(newValue)
   
   
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeft} text='left' />
-      <Button handleClick={handleRight} text='right' />
-      {right}
-      <History allClicks={allClicks}/>
+      {value}
+      <button onClick={setToValue(0)} >zero</button>
+      <button onClick={setToValue(100)} >hundred</button>
+      <button onClick={setToValue(value + 1)} >increment button</button>
     </div>
   )
 }
