29a669cb413a46f5f4abdb3afc952334b4da82a1
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-04-03 10:17:08 -0400

    add components folder

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Note from "./components/Note";
+function App({ notes }) {
+  const [newNotes, setNewNotes] = useState(notes);
 
-function App() {
+  const [newNote, setNewNote] = useState("a new note");
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const addNote = (event) => {
+    event.preventDefault();
+    setNewNotes(
+      newNotes.concat({
+        id: newNotes.length + 1,
+        content: newNote,
+        important: Math.random() < 0.5,
+      })
+    );
+  };
 
   return (
-<>
-hello
+    <>
+      <ul>
+        {newNotes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input type="text" value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
